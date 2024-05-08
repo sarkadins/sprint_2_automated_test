b44e6472f9b6383092fe175a6cfbe706c8d22dc7
bftsgt <bfutasz@gmail.com>
2024-05-08 14:20:57 +0200

    thread.sleeps added temporarily

--- src/test/java/org/example/BrowseIssue.java
+++ src/test/java/org/example/BrowseIssue.java
@@ -23,33 +23,45 @@ public class BrowseIssue {
         driver.findElement(By.id("login-form-submit")).click();
     }
     @Test
-    void testBrowseToucanIssue() {
+    void testBrowseToucanIssue() throws InterruptedException {
         driver.findElement(By.id("find_link")).click();
+        Thread.sleep(500);
+
         driver.findElement(By.id("issues_new_search_link_lnk")).click();
+        Thread.sleep(500);
         driver.findElement(By.id("searcher-query")).sendKeys("toucan");
         driver.findElement(By.className("search-button")).click();
+        Thread.sleep(500);
         WebElement toucanMessage = driver.findElement(By.className("issue-link-summary"));
         String actual = toucanMessage.getText();
         String expected = "TOUCAN project issue";
         assertEquals(expected, actual);
     }
     @Test
-    void testBrowseJetiIssue(){
+    void testBrowseJetiIssue() throws InterruptedException {
         driver.findElement(By.id("find_link")).click();
+        Thread.sleep(500);
         driver.findElement(By.id("issues_new_search_link_lnk")).click();
+        Thread.sleep(500);
         driver.findElement(By.id("searcher-query")).sendKeys("jeti");
+        Thread.sleep(500);
         driver.findElement(By.className("search-button")).click();
+        Thread.sleep(500);
         WebElement jetiKey = driver.findElement(By.className("issue-link-key"));
         String actual = jetiKey.getText();
         String expected = "JETI-731";
         assertEquals(expected, actual);
     }
     @Test
-    void testBrowseCoalaIssue(){
+    void testBrowseCoalaIssue() throws InterruptedException {
         driver.findElement(By.id("find_link")).click();
+        Thread.sleep(500);
         driver.findElement(By.id("issues_new_search_link_lnk")).click();
+        Thread.sleep(500);
         driver.findElement(By.id("searcher-query")).sendKeys("coala");
+        Thread.sleep(500);
         driver.findElement(By.className("search-button")).click();
+        Thread.sleep(500);
         WebElement coalaKey = driver.findElement(By.className("issue-link-key"));
         String actual = coalaKey.getText();
         String expected = "COALA-695";
