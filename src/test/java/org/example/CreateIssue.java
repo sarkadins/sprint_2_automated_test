39806988af761872bb824847c5d2521b014c4095
bftsgt <bfutasz@gmail.com>
2024-05-08 14:10:18 +0200

    create issue as Toucan fixed

--- src/test/java/org/example/CreateIssue.java
+++ src/test/java/org/example/CreateIssue.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CreateIssue {
     WebDriver driver;
@@ -25,12 +29,27 @@ public class CreateIssue {
     @Test
     void testCreateIssueAsToucan() throws InterruptedException {
         driver.findElement(By.xpath("//*[@id=\"create_link\"]")).click();
-        Thread.sleep(500);
+        Thread.sleep(700);
+        driver.findElement(By.cssSelector("#project-field")).click();
+        driver.findElement(By.xpath("//ul[@id='all-projects']/li[6]/a[@role='presentation']")).click();
+
+        Thread.sleep(700);
         driver.findElement(By.xpath("//*[@id=\"summary\"]")).sendKeys("test summary");
+        Thread.sleep(700);
         driver.findElement(By.xpath("//*[@id=\"create-issue-submit\"]")).click();
-        Thread.sleep(500);
+        Thread.sleep(700);
         Assertions.assertTrue(driver.findElement(By.xpath("//*[@id=\"aui-flag-container\"]/div/div/a")).isDisplayed());
         Thread.sleep(1200);
+    }
+
+    @Test
+    void testCreateIssueAsJeti() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"create_link\"]")).click();
+        Thread.sleep(700);
+        driver.findElement(By.cssSelector("#project-field")).click();
+        assertThrows(NoSuchElementException.class, () -> {
+            driver.findElement(By.linkText("JETI project (JETI)")).click();
+        });
     }
 
     @AfterEach
