7cd825966a6b4b75e573491a90778a00e189a9a5
sarkadins <soma.sarkadi@gmail.com>
2024-05-09 13:47:30 +0200

    implement verifyCreateIssuePermission method within PermissionWithGlass Class

--- src/test/java/org/example/PermissionWithGlass.java
+++ src/test/java/org/example/PermissionWithGlass.java
@@ -1,11 +1,13 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -45,5 +47,23 @@ public class PermissionWithGlass {
 
         Assertions.assertEquals("Any logged in user", driver.findElement(By.cssSelector("#project-config-panel-permissions > jira-permissions-table > div:nth-child(1) > table > tbody > tr:nth-child(2) > td.grants > dl > dd"))
                 .getText());
+    }
+    @Test
+    void verifyCreateIssuePermission() {
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html//a[@id='browse_link']"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Use It To Practice Project (PP)"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html//section[@id='sidebar']//a[@href='/plugins/servlet/project-config/PP']")))
+                .click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html//a[@id='view_project_permissions']")))
+                .click();
+        WebElement permission = driver.findElement(By.cssSelector("div:nth-of-type(2) > .aui.jira-admin-table > tbody > tr:nth-of-type(4) > .grants  dd"));
+        String actual = permission.getText();
+        String expected = "Any logged in user";
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @AfterEach
+    void tearDown(){
+        driver.quit();
     }
 }
